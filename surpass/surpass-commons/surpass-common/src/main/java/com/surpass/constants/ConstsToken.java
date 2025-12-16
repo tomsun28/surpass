7754b93916981dbede8ed7121d5db63e1ad8779e
shimingxy@qq.com <shimingxy@qq.com>
2025-12-16 18:08:26 +0800

    token

---
+++ surpass/surpass-commons/surpass-common/src/main/java/com/surpass/constants/ConstsToken.java
@@ -0,0 +1,17 @@
+package com.surpass.constants;
+
+public class ConstsToken {
+	public static final String CLIENT_ID 				= "client_id";
+	
+	public static final String CLIENT_SECRET 			= "client_secret";
+	
+	public static final String GRANT_TYPE               = "grant_type";
+	
+	public static final String ACCESS_TOKEN           	= "access_token";
+	
+	public static final String EXPIRES_IN               = "expires_in";
+	
+	public static final String REFRESH_TOKEN			= "refresh_token";
+	
+	public static final String REFRESH_TOKEN_EXPRISE    = "refresh_token_exprise";
+}
--- surpass/surpass-web-api/src/main/java/com/surpass/web/controller/TokenEndpoint.java
+++ surpass/surpass-web-api/src/main/java/com/surpass/web/controller/TokenEndpoint.java
@@ -1,19 +1,23 @@
 package com.surpass.web.controller;
 
+import com.surpass.constants.ConstsToken;
+import com.surpass.entity.AccessToken;
 import com.surpass.entity.Message;
 import com.surpass.entity.RegisteredClient;
 import com.surpass.persistence.service.RegisteredClientService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.web.bind.annotation.*;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @description:
  * @author: orangeBabu
  * @time: 2025/11/26 15:29
+ * @author shimingxy modify
  */
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class TokenEndpoint {
@@ -21,21 +25,18 @@ public class TokenEndpoint {
     private final RegisteredClientService clientService;
 
     @PostMapping("/token")
-    public Message<Map<String, Object>> getToken(@RequestParam String clientId,
-                                                 @RequestParam String clientSecret) {
-
-    /*    App app = appService.findByClientId(clientId);
-        if(app == null || !BCrypt.checkpw(clientSecret, app.getClientSecret())){
-            return new Message<>(HttpStatus.UNAUTHORIZED.value(), HttpStatus.UNAUTHORIZED.getReasonPhrase());
-        }
-
-        String token = appService.issueToken(app);*/
+    public AccessToken getToken(
+    			@RequestParam(ConstsToken.CLIENT_ID) String clientId,
+    			@RequestParam(ConstsToken.CLIENT_SECRET) String clientSecret,
+    			@RequestParam(ConstsToken.GRANT_TYPE) String grantType,
+    			@RequestParam(ConstsToken.REFRESH_TOKEN) String refreshToken) {
     	RegisteredClient client = clientService.findByClientId(clientId);
-
-        Map<String,Object> result = new HashMap<>();
-//        result.put("access_token", token);
-        result.put("token_type", "Bearer");
-//        result.put("expires_in", app.getTtlSeconds());
-        return Message.ok(result);
+    	
+    	AccessToken accessToken = new AccessToken();
+    	if(client == null) {
+    		accessToken.setMessage(Message.FAIL ,"invalid_client_id");
+    		
+    	}
+        return accessToken;
     }
 }
